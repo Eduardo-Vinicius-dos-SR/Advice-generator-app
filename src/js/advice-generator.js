75d3c68b63dd0ddff29c134304eb4600b467756d
Eduardo-Vinicius-dos-SR <eduardovdsr@gmail.com>
2025-03-25 17:41:43 -0300

    resolvendo conflitos

--- src/js/advice-generator.js
+++ src/js/advice-generator.js
@@ -2,7 +2,16 @@ const adviceId = document.getElementById("advice-id");
 const adviceText = document.getElementById("advice-text");
 
 async function getAdvice() {
+<<<<<<< HEAD
 	const response = await fetch("https://api.adviceslip.com/advice");
+=======
+	const response = await fetch("https://api.adviceslip.com/advicse");
+	if (!response.ok) {
+		adviceId.innerText = `Advice #000`;
+		adviceText.innerText = `There was a problem generating the advice, please try again`;
+		throw new Error("There was a problem generating the advice");
+	}
+>>>>>>> d3fdec7 (coletando dados)
 	return await response.json();
 }
 
